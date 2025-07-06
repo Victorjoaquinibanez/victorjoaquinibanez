8b361af352f034ceb8a92e906980fe52a3b26ab6
Victorjoaquinibanez <victorjoaquinibanez@gmail.com>
2025-07-06 17:25:46 -0300

    nuevo codigo error

---
+++ error.py
@@ -0,0 +1,224 @@
+# Escribe un programa que que solicite una puntuacion entre 0.0 1.0. Si la puntuacion esta fuera de ese rango, muestra error. Si la puntuacion esta entre 0.0 y 1.0 muestra calificacion usando la tabla siguiente:
+#>=0.9 sobresaliente
+#>=0.8 notable
+#>=0.7 bien
+#>=0.6 suficiente
+#<0.6 insuficiente
+
+# puntuacion = float(input("ingresa una puntuacion entre 0.0 y 1.0:\n"))
+# if puntuacion > 1.0:
+#     print("error")
+# else:
+#     if puntuacion >= 0.9:
+#         print("sobresaliente")
+#     else:
+#         if puntuacion >= 0.8:
+#             print("notable")
+#         else:
+#             if puntuacion >= 0.7:
+#                 print("bien")
+#             else:
+#                 if puntuacion >= 0.6:
+#                     print("sufuciente")
+#                 else:
+#                     if puntuacion < 0.6:
+#                         print("insuficiente")
+             
+#def muestra_estribillo():
+ #   print(" soy leñador, que alegria.")
+  #  print(" duermo toda la noche y trabajp todo el dia.")
+
+# def fred():
+#     print("zap")
+# def jane():
+#     print("ABC")
+# jane()
+# fred()
+# jane()
+
+
+# horas=int(input("ingrese la cantidad de horas:"))
+# tarifas=10
+# if horas>40:
+#     salario=40*tarifas
+#     extra=(horas-40)*tarifas*1.5
+#     salario=salario+extra
+# else:
+#     salario=horas*tarifas
+# print(salario)
+
+#puntuacion = float(input("ingresa una puntuacion entre 0.0 y 1.0:\n"))
+# def calculo_calificacion(puntaje):
+#     if puntaje > 1.0: and puntaje <=0.6
+#     else:
+#      if puntaje >= 0.9:
+#          print("sobresaliente")
+#      else:
+#          if puntaje >= 0.8:
+#              print("notable")
+#          else:
+#              if puntaje >= 0.7:
+#                  print("bien")
+#              else:
+#                  if puntaje >= 0.6:
+#                      print("sufuciente")
+#                  else:
+#                      if puntaje < 0.6:
+#                          print("insuficiente")
+
+# mayor= None
+# print("antes:", mayor)
+# for valor in[3,41,12,9,74,15]:
+#     if mayor is None or valor>mayor:
+#         mayor=valor
+#         print("bucle", valor, mayor)
+#         print("mayor", mayor)
+
+#4_ Dados dos numeros a y B imprime el mayor de ellos
+# a=int(imput("ingrese un numero"/n))
+# b=int(imput("ingresa otro numero"/n))
+# if 
+#   a>b
+#   print(a)
+# else
+#   print("es menor")
+
+# lados=int (input("ingresa la cantidasd de lados:\n"))
+# match lados:
+#      case "3":
+#          print("triangulo")
+#      case "4":
+#          print("cuadrilatero")
+#      case "5":
+#          print("pentagono")
+#      case _:
+#          print("error")
+
+# lados=int(input("Ingresa cantidad De Lados:\n"))
+# if lados==3:
+#     print("Es un Triangulo")
+# else:
+#     if lados==4:
+#         print("Es Cuadrilatero")
+#     else:
+#         if lados==5:
+#             print("Es un Pentagono")
+#         else:
+#             print("No es Ninguno")
+
+# A=int(input("Ingresa A:\n"))
+# B=int(input("Ingresa B:\n"))
+# C=int(input("ingresa C:\n"))
+# if A==B==C:
+#     print("No hay numero mayor")
+# else:
+#     if A>B ^ A>C:
+#         print(A)
+#     else:
+#        if B>A ^ B>C:
+#           print(B)
+#        else:
+#          print(C)
+
+# Sp= 0
+# Sn= 0
+# I= 0
+# while I<= 4:
+#     N= int(input("Ingrese un numero: \n"))
+#     if N >= 0:
+#         Sp=Sp+N
+#     else:
+#         Sn=Sn+N
+#     I=I+1
+# print(Sp)
+# print(Sn)
+
+
+# Sp= 0
+# Sn= 0
+# Nu= 0
+# I= 0
+# while I<= 4:
+#     N= int(input("Ingrese un numero:\n"))
+#     if N== 0:
+#         Nu=Nu+1
+#     else:
+#         if N> 0:
+#             Sp=Sp+1
+#         else:
+#             Sn=Sn+1
+#     I=I+1
+# print("contador de positivos",Sp)
+# print("contador de negativos",Sn)
+# print("contador de nulos",Nu)
+
+# Pro= 0
+# Sum= 0
+# I= 1
+# while I<=4:
+    
+#     N= int(input("Ingrese un Numero:\n"))
+#     Sum=Sum+N
+#     I=I+1
+# Pro=Sum/4
+# print("Promedio:",Pro)
+
+# Pro= 0
+# Nul= 0
+# Sp= 0
+# Pos= 0
+# I= 1
+# while I<=4:
+#     N= int(input("Ingrese un Numero:\n"))
+#     if N==0:
+#        Nul=Nul+1
+#     else:
+#       if N>0:
+#         Sp=Sp+N
+#         Pos=Pos+1
+#       else:
+#         print("Negativos",N)
+#     I=I+1
+# Pro=Sp/Pos
+# print("promedio",Pro)
+# print("nulos", Nul)
+
+# Np=0
+# Ni=0
+# I=1
+# while I<=5:
+#     N=int(input("Ingrese un Numero:\n"))
+#     Mod=N%2
+#     if Mod==0:
+#         Np=Np+N
+#     else:
+#         Ni=Ni+N
+#     I=I+1
+# print("Numeros pares",Np)
+# print("Numeros Impares", Ni)
+
+# A1=0
+# A2=0
+# B1=0
+# C=0
+# A= int(input("Ingras un Numero A: \n"))
+# B= int(input("Ingras un Numero B: \n"))
+# A1=A/100
+# A2=A-A1
+# B1=B/100
+# C=A2+B1
+# print("El Numero es:\n",C)
+
+# Np=0
+# I=1
+# while I<=8:
+#     N=int(input("Ingrese Un Numero:\n"))
+#     mod=I%2
+#     if mod==0:
+#         Np=Np+I
+#     else:
+#      I=I+1
+# print("Suma De Numeros Pares:",Np)
+
+# num=int(input("ingresa un numero:\n"))
+# int vec(num)
